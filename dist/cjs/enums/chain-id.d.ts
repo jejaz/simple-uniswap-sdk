1d4dd1efba2ce761228349c5dcab38545685db62
jejaz <jelenazigic777@gmail.com>
2022-03-08 11:28:27 +0100

    dist

---
+++ dist/cjs/enums/chain-id.d.ts
@@ -0,0 +1,9 @@
+export declare enum ChainId {
+    MAINNET = 1,
+    ROPSTEN = 3,
+    RINKEBY = 4,
+    GÖRLI = 5,
+    KOVAN = 42,
+    POLYGON = 80001
+}
+export declare const ChainNames: Map<number, string>;
---
+++ dist/esm/enums/chain-id.js
@@ -0,0 +1,17 @@
+export var ChainId;
+(function (ChainId) {
+    ChainId[ChainId["MAINNET"] = 1] = "MAINNET";
+    ChainId[ChainId["ROPSTEN"] = 3] = "ROPSTEN";
+    ChainId[ChainId["RINKEBY"] = 4] = "RINKEBY";
+    ChainId[ChainId["G\u00D6RLI"] = 5] = "G\u00D6RLI";
+    ChainId[ChainId["KOVAN"] = 42] = "KOVAN";
+    ChainId[ChainId["POLYGON"] = 80001] = "POLYGON";
+})(ChainId || (ChainId = {}));
+export var ChainNames = new Map([
+    [ChainId.MAINNET, 'mainnet'],
+    [ChainId.ROPSTEN, 'ropsten'],
+    [ChainId.RINKEBY, 'rinkeby'],
+    [ChainId.GÖRLI, 'görli'],
+    [ChainId.KOVAN, 'kovan'],
+    [ChainId.POLYGON, 'polygon']
+]);
