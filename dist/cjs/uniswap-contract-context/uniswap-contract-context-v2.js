d99eb48bad317a43336797e917a9775124a59ff6
jejaz <jelenazigic777@gmail.com>
2022-03-25 09:30:33 +0100

    address for mumbai

--- dist/cjs/uniswap-contract-context/uniswap-contract-context-v2.js
+++ dist/cjs/uniswap-contract-context/uniswap-contract-context-v2.js
@@ -15,7 +15,10 @@ var UniswapContractContextV2 = /** @class */ (function () {
     /**
      * The uniswap pair address
      */
-    UniswapContractContextV2.pairAddress = '0x9b967c6db1a8cc1e73eca79ca3e5b32ba9ac6465';
+    //address for polygon mainnet
+    //public static pairAddress = '0x9b967c6db1a8cc1e73eca79ca3e5b32ba9ac6465';
+    //address for mumbai
+    UniswapContractContextV2.pairAddress = '0xc35dadb65012ec5796536bd9864ed8773abc74c4';
     /**
      * Uniswap v2 router
      */
